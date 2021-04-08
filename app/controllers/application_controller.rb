9561cdb0169f75e1c5d91f068de3a8b25cc440d6
speraza545 <s.peraza545@gmail.com>
2021-04-07 20:28:10 -0400

    commit tree 75f56c237ccff737228d9790052344b58c358d35

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,12 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-
+    if params[:username] = "" || params[:password] = ""
+      redirect "/failure"
+    else 
+      User.create(username: params[:username], password: params[:password])
+      redirect "/login"
+    end
   end
 
   get '/account' do
@@ -33,7 +38,14 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-  end
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect to "/account"
+      else
+        redirect to "/failure"
+      end
+    end
 
   get "/failure" do
     erb :failure
